5adc4d55e1f2d56d442cc009ca8511ffab5d1f92
Vivek Singh <iamvivekrajawat@gmail.com>
2025-08-31 10:25:23 +0530

    backend api

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
-import express, { urlencoded } from "express"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-import cors from "cors"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import adminRouter from "./routes/adminRoutes.js";
 import batchRouter from "./routes/batchRoutes.js";
 import teacherRoute from "./routes/teacherroutes.js";
 import submissionRouter from "./routes/submissionRoutes.js";
 import noticeRouter from "./routes/NoticeRoute.js";
+
 dotenv.config();
-const app = express()
-mongoose.connect(process.env.DB_URL).then((result) => {
-    app.use("/uploads/profile", express.static("uploads/profile"));
-    app.use("/assignment/files", express.static("assignment/files"));
-    app.use(express.json())
-    app.use(cors());
-    app.use(express.urlencoded({ extended: true }))
-    app.use('/batch', batchRouter)
-    app.use('/admin',adminRouter)
-    app.use('/teacher', teacherRoute)
-    app.use('/student', submissionRouter)
-    app.use('/notice',noticeRouter)
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/uploads/profile", express.static("uploads/profile"));
+app.use("/assignment/files", express.static("assignment/files"));
+
+app.use("/batch", batchRouter);
+app.use("/admin", adminRouter);
+app.use("/teacher", teacherRoute);
+app.use("/student", submissionRouter);
+app.use("/notice", noticeRouter);
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+// Database + Server
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
     app.listen(3000, () => {
-        console.log("Server  Started")
-    })
-}).catch((err) => {
-    console.log("Server is not connected something is error", err)
-})
+      console.log("Server Started on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(" DB Connection Error:", err);
+  });
